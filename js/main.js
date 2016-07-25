abdb77e5ed5f7472766bfd60e04bb5dd798c3e90
Piranut <lapprathana@gmail.com>
2016-07-25 21:11:58 +0700

    new website

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(".animsition").animsition({
+    inClass: 'fade-in',
+    outClass: 'fade-out',
+    linkElement: '.animsition-link',
+    inDuration: 2200,
+    outDuration: 500
+});
+
+$('.image a').vanillabox({
+    type: 'image',
+    adjustToWindow: 'both'
+});
+
